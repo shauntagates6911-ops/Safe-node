b298636f87493945d2cc23bd845a6b70c48f1099
shauntagates6911-ops <shaunta.gates6911@gmail.com>
2026-09-05 10:55:57 -0400

    Add warning message in main.dart

---
+++ Warnings/main.dart
@@ -0,0 +1,3 @@
+void main() {
+  print("If you dont know what this site does, EXIT NOW! People can upload inapropriate things, post projects on your account, and get you BANNED from Safe-node!");
+}
